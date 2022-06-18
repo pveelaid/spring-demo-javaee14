8d54714e09c5275f00b49a3a3ab70e5923f461b1
pveelaid <pveelaid@gmail.com>
2022-06-18 14:00:54 +0300

    [change] - adding some comments for code and first service created

--- src/main/java/com/sda/springdemojavaee14/controller/MyFirstRestController.java
+++ src/main/java/com/sda/springdemojavaee14/controller/MyFirstRestController.java
@@ -5,15 +5,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//@RestController creates new Spring bean base on the class
+//bean = component = managed object(object created by Spring for us)
 @RestController
 public class MyFirstRestController {
 
     private static final Logger log = LoggerFactory.getLogger(MyFirstRestController.class);
 
+    //@GetMapping means - code of the method will be run when we hit endpoint, we use exact HTTP method
     @GetMapping("/say-hello")
     public String hello(){
         // TODO : explain logger level
-        log.info("Hello method was called..");
+        log.info("Hello method was called.");
 
 
         return "Welcome to spring class!";
