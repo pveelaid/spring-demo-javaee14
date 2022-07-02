f2a134995da2b05cceabd534d9b55b3e9e47f54d
pveelaid <pveelaid@gmail.com>
2022-07-02 11:10:08 +0300

    [change] - using @Scope(SCOPE_PROTOTYPE) to provide all wheels

--- src/main/java/com/sda/springdemojavaee14/homework/ex1/Wheel.java
+++ src/main/java/com/sda/springdemojavaee14/homework/ex1/Wheel.java
@@ -1,8 +1,12 @@
 package com.sda.springdemojavaee14.homework.ex1;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
+
+@Component("ratas")
+@Scope(SCOPE_PROTOTYPE)
 public class Wheel {
     public Wheel() {
         System.out.println("Creating new wheel");
