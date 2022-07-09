1991e44365ee4324dac2458489d8f9e4cc73341c
pveelaid <pveelaid@gmail.com>
2022-07-09 11:06:29 +0300

    [change] - adding in memory repository of reservations and using this with connection of ReservationService

--- src/main/java/com/sda/springdemojavaee14/service/ReservationService.java
+++ src/main/java/com/sda/springdemojavaee14/service/ReservationService.java
@@ -1,6 +1,7 @@
 package com.sda.springdemojavaee14.service;
 
 import com.sda.springdemojavaee14.entity.Reservation;
+import com.sda.springdemojavaee14.repository.ReservationRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -10,24 +11,16 @@ import java.util.List;
 @Service
 @Slf4j
 public class ReservationService {
-public List<Reservation> findAllReservation(){
+
+    private final ReservationRepository reservationRepository;
+
+    public ReservationService(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
+
+    public List<Reservation> findAllReservation(){
     log.info("finding all reservations");
 
-    return List.of(
-            new Reservation(1L, 12L, LocalDateTime.now(), LocalDateTime.now().plusHours(2),
-                    "Wołek", "51124", "m@op.pl", 10, ""),
-
-            Reservation.builder()
-                    .id(2L)
-                    .tableNumber(12L)
-                    .startBookingTime(LocalDateTime.now())
-                    .endBookingTime(LocalDateTime.now().plusHours(2))
-                    .surname("Veelaid")
-                    .phoneNumber("555 555 555")
-                    .email("mvee@sda.pl")
-                    .numberOfPeople(5)
-                    .address("Pikk 5")
-                    .build()
-    );
+        return reservationRepository.findAllReservationInRepository();
 }
 }
