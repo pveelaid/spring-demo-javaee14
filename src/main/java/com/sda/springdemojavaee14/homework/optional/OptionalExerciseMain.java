34bf9a9c273ade1db42ff7d7b3d6336924af546c
pveelaid <pveelaid@gmail.com>
2022-07-16 11:08:38 +0300

    [change] - ifPresent with my own lambda

--- src/main/java/com/sda/springdemojavaee14/homework/optional/OptionalExerciseMain.java
+++ src/main/java/com/sda/springdemojavaee14/homework/optional/OptionalExerciseMain.java
@@ -1,6 +1,7 @@
 package com.sda.springdemojavaee14.homework.optional;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 
 public class OptionalExerciseMain {
 
@@ -16,6 +17,26 @@ public class OptionalExerciseMain {
             System.out.println("Sorry, no nick");
         }
 
+        if(maybeNick.isEmpty()){
+            System.out.println("Sorry no, nick");
+        } else {
+            System.out.println(maybeNick.get());
+        }
+
+        System.out.println("Better usage of Optional");
+        System.out.println("nick: " + maybeNick.orElse("no nick"));
+
+        System.out.println("Optional with lambda");
+        maybeNick.ifPresent(new MyNickConsumer());
+
+        System.out.println("Now with my own lambda");
+        //lambda is about reducing ceremony
+        //lambda is for shorter way of providing interface implementation
+        //lambda is content of missing interface method
+        //we don't need argument type in lambda because we know which method we implement
+        //-> (arrow) is separator of arguments and method body
+        maybeNick.ifPresent(nick -> System.out.println("Nick is: " + nick));
+
     }
     // In Kotlin Language: String (never null) and String?
     public static String personNick() {
@@ -26,6 +47,15 @@ public class OptionalExerciseMain {
     // we're using "factory methods" for creating instances of this class
     // factory method - is static method which is able to produce instances of classes and have good name
     public static Optional<String> personNickIfPresent() {
-        return Optional.empty();
+        return Optional.ofNullable("peeter");
+    }
+}
+
+//generic interfase consumer  - T will be replaced with string in our case
+class MyNickConsumer implements Consumer<String>{
+
+    @Override
+    public void accept(String t) {
+        System.out.println("Nick is: " + t);
     }
 }
