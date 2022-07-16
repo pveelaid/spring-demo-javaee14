e7953e3f1acb06394eb2e45ba8e2e338749740cf
pveelaid <pveelaid@gmail.com>
2022-07-16 12:53:44 +0300

    [change] - findReservationByiD first approach

--- src/main/java/com/sda/springdemojavaee14/service/ReservationService.java
+++ src/main/java/com/sda/springdemojavaee14/service/ReservationService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -26,6 +27,11 @@ public class ReservationService {
 
 
     public Reservation findReservationById(Long reservationId) {
-        return null;
+        log.info("finding reservation with id: [{}]", reservationId);
+       //Optional<Reservation> reservationFromDb = reservationRepository.findById(reservationId);
+        //var is for less typing and replaces all type name - the same as above line
+        //if you use var provide good name - always try to provide good name
+        var reservationFromDb = reservationRepository.findById(reservationId);
+        return reservationFromDb.orElse(null);
     }
 }
