7a7720b11dbd2506ea6f8237c944572e9bae75bb
pveelaid <pveelaid@gmail.com>
2022-07-30 09:20:25 +0300

    [add] - reservation page in progress

--- src/main/java/com/sda/springdemojavaee14/repository/ReservationRepository.java
+++ src/main/java/com/sda/springdemojavaee14/repository/ReservationRepository.java
@@ -4,7 +4,7 @@ import com.sda.springdemojavaee14.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository extends
-        //first argument is Entiry, second is type of primary key
+        //first argument is Entity, second is type of primary key
         JpaRepository<Reservation, Long> {
 
 }
