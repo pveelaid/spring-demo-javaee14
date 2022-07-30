6ddf855cc37494d186bbd31648f3bcdd7f5a9f02
pveelaid <pveelaid@gmail.com>
2022-07-30 10:30:00 +0300

    [add] - reservations page in progress

--- src/main/java/com/sda/springdemojavaee14/controller/web/ReservationWebController.java
+++ src/main/java/com/sda/springdemojavaee14/controller/web/ReservationWebController.java
@@ -35,7 +35,7 @@ public class ReservationWebController {
         var reservationsFromDB = reservationService.findAllReservations();
         data.addAttribute(RESERVATIONS_KEY, reservationsFromDB);
 
-        return "all-reservations";
+        return "reservations/all-reservations";
     }
 
 }
