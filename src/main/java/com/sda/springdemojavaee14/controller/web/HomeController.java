ae4a30734b6ba1d181209b6d6df9f1d5a1f41340
pveelaid <pveelaid@gmail.com>
2022-07-30 11:37:15 +0300

    [add] - homepage created

---
+++ src/main/java/com/sda/springdemojavaee14/controller/web/HomeController.java
@@ -0,0 +1,13 @@
+package com.sda.springdemojavaee14.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "/home"})
+    public String homePage(){
+        return "index";
+    }
+}
